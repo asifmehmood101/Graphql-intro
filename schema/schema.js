673ef59769de089f17c8b9b716861e9b1feb5efc
asifmehmood101 <amehmood999@gmail.com>
2021-01-11 12:32:58 +0500

    Grapqhl-intro-first commit

---
+++ schema/schema.js
@@ -0,0 +1,135 @@
+const grapql = require('graphql');
+const axios = require('axios');
+
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull,
+} = grapql;
+
+//CompanyType
+
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parentValue, args) {
+                return axios
+                    .get(
+                        `http://localhost:3000/companies/${parentValue.id}/users`
+                    )
+                    .then(res => res.data);
+            },
+        },
+    }),
+});
+
+//User-Schema
+const UserType = new GraphQLObjectType({
+    //schema name
+    name: 'User',
+    //schema fields
+    fields: {
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: {
+            type: GraphQLInt,
+        },
+        company: {
+            type: CompanyType,
+            resolve(parentValue, args) {
+                return axios
+                    .get(
+                        `http://localhost:3000/companies/${parentValue.companyId}`
+                    )
+                    .then(res => res.data);
+            },
+        },
+    },
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+                return axios
+                    .get(`http://localhost:3000/users/${args.id}`)
+                    .then(res => res.data);
+            },
+        },
+        company: {
+            type: CompanyType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+                return axios
+                    .get(`http://localhost:3000/companies/${args.id}`)
+                    .then(res => res.data);
+            },
+        },
+    },
+});
+
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addUser: {
+            type: UserType,
+            args: {
+                firstName: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) },
+                companyId: { type: GraphQLString },
+            },
+            resolve(parentValue, { firstName, age }) {
+                return axios
+                    .post(`http://localhost:3000/users`, {
+                        firstName,
+                        age,
+                    })
+                    .then(res => res.data);
+            },
+        },
+        DeleteUser: {
+            type: UserType,
+            args: { id: { type: new GraphQLNonNull(GraphQLString) } },
+            resolve(parentValue, { id }) {
+                return axios
+                    .delete(`http://localhost:3000/users/${id}`)
+                    .then(res => res.data);
+            },
+        },
+        EditUser: {
+            type: UserType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLString) },
+                firstName: { type: GraphQLString },
+                age: { type: GraphQLInt },
+                companyId: { type: GraphQLString },
+            },
+            resolve(parentValue, { id, firstName, age, companyId }) {
+                return axios
+                    .patch(`http://localhost:3000/users/${id}`, {
+                        id,
+                        firstName,
+                        age,
+                        companyId,
+                    })
+                    .then(res => res.data);
+            },
+        },
+    },
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+    mutation: mutation,
+});
